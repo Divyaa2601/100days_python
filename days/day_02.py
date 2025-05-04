872f59510eec34ef6256c4524016d7c75082c2ed
Divyaa2601 <153746884+Divyaa2601@users.noreply.github.com>
2025-05-04 11:35:33 +0530

    Create day_02.py

---
+++ days/day_02.py
@@ -0,0 +1,7 @@
+name = input("What's you name?: ")
+food = input("What's your favourite food?: ")
+music = input("What's your favourite music?: ")
+location = input("Where do you live?: ")
+
+print()
+print(f"You are {name}.You're probably hungry for {food} and you're definitely getting your groove on to {music} living in the amazing {location}.")
