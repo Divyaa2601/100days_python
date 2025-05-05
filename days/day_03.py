c336e05a68ba41904609ce690adbb52902f9244d
Divyaa2601 <153746884+Divyaa2601@users.noreply.github.com>
2025-05-05 13:14:50 +0530

    Create day_03.py

---
+++ days/day_03.py
@@ -0,0 +1,8 @@
+food=input("Name a food: ")
+plant=input("Name a plant: ")
+cooking=input("Name a cooking method: ")
+burned=input("Name a burnt food: ")
+item=input("Name a DIY item: ")
+print()
+print("MENU")
+print(cooking,food ,"with",burned,plant ,"on a bed of",item)
